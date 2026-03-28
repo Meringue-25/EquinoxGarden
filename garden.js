f49cad6dfa75ce088851ad454d6aa5f1f1394228
alexislam735-cell <alexislam735@gmail.com>
2026-03-28 22:20:04 +0000

    added imagery with button thingy

--- garden.js
+++ garden.js
@@ -1,6 +1,16 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+let currentStage = 1; // Starting stage
 
+function cycleBlossoms() {
+    currentStage++;
+    if (currentStage > 7) {
+        currentStage = 1;
+    }
+    const blossomImg = document.getElementById("blossom-img");
+    blossomImg.src = `imgs/Cherry Blossem-${currentStage}.png.png`;
+    blossomImg.alt = `Cherry blossom stage ${currentStage}`;
+}
 
 function addTask()  {
 
@@ -23,4 +33,3 @@ function addTask()  {
     listContainer.appendChild(li);
     inputBox.value = "";
 }
-
