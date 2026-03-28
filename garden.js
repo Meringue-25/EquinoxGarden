20ea8eef57a0bf8f28702aa36a87bf21440e1580
Meringue-25 <yuki.li3465@gmail.com>
2026-03-28 14:32:22 -0700

    fixed the div tags

--- garden.js
+++ garden.js
@@ -3,11 +3,13 @@ const listContainer = document.getElementById("list-container");
 
 
 function addTask()  {
+    
     const task = inputBox.value.trim();
     if (!task) {
         alert("Please write down a task");
         return; 
     }
+
     const li = document.createElement("li");
     li.innerHTML = `
          <label>
