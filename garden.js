8da03fcc3bb43708a29eb10fe809150d34985b05
alexislam735-cell <alexislam735@gmail.com>
2026-03-28 23:33:20 +0000

    java for tree

--- garden.js
+++ garden.js
@@ -1,6 +1,17 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+let currentStage = 1; // Starting stage
+
+function cycleBlossoms() {
+    currentStage++;
+    if (currentStage > 7) {
+        currentStage = 1;
+    }
+    const blossomImg = document.getElementById("blossom-img");
+    blossomImg.src = `imgs/Cherry Blossem-${currentStage}.png.png`;
+    blossomImg.alt = `Cherry blossom stage ${currentStage}`;
+}
 
 function addTask()  {
 
